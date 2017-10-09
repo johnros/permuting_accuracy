d7ffa279fcf1bc1b286c186ac3d08e6926c75d30
Jonathan Rosenblatt <johnros@bgu.ac.il>
2017-10-09 13:43:35 +0300

    synch

--- Round1/Gilron Effect of Cross Validatino.R
+++ Round1/Gilron Effect of Cross Validatino.R
@@ -188,6 +188,17 @@ t_Hotelling <- function(x,y, shrinkage){
 
 
 
+# Goeman's high-dim test
+t_goeman <- function(x,y){
+  X <- rbind(x,y)
+  dimnames(X) <- list(NULL,LETTERS[1:ncol(X)])
+  y <- as.matrix(c(rep(FALSE, nrow(x)),rep(TRUE,nrow(y))))
+  result <- globaltest::gt(y,X)
+  result@result[,'Statistic'] %>% unname()
+}
+## Testing
+# t_goeman(x1,x2)
+
 
 
 # Linear SVM. 
