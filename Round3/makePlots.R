7e9d31fcb98cada719ec99820d07f5f6815c047f
Jonathan Rosenblatt <johnros@bgu.ac.il>
2018-04-20 20:36:47 +0300

    progress

--- Round3/makePlots.R
+++ Round3/makePlots.R
@@ -1,7 +1,13 @@
+ksource <- function(x, ...) {
+  library(knitr)
+  source(purl(x, output = tempfile()), ...)
+}
+
+
 setwd('~/workspace/permuting_accuracy/Round3/')
 files <- list.files(pattern = '.Rmd')
 base::sort(files)
 for(.file in files){
   # .file <- files[[2]]
-  source(.file)
+  ksource(.file)
 }
