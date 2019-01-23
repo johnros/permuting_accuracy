6eec0bd7813fed25b021cb1d7fe12b0887229de2
Jonathan Rosenblatt <johnros@bgu.ac.il>
2019-01-23 14:25:11 +0200

    code for round 4

---
+++ Round4/MakeFile.R
@@ -0,0 +1,21 @@
+ksource <- function(x, ...) {
+  library(knitr)
+  source(purl(x, output = tempfile()), ...)
+}
+
+# Automate #####
+
+# Remove existing files
+list.files("~/workspace/permuting_accuracy/Round3/Output/", full.names = TRUE) %>%
+  list %>%
+  do.call(file.remove,.)
+
+# Run with exceptions
+files <- list.files('~/workspace/permuting_accuracy/Round3', pattern = '.Rmd', full.names = TRUE)
+exceptions <- c('file1.Rmd', 'file39.Rmd')
+files <- files[!grepl(exceptions, files, fixed = TRUE)]
+
+for(.file in files){
+  try({ksource(.file))
+}
+pbPost(type = "note", body = "Done", "All files finished")
