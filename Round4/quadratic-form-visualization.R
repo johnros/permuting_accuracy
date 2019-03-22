ed1f6c498af8b3aa33893ea6050100270e9f6c3a
Jonathan Rosenblatt <johnros@bgu.ac.il>
2019-03-22 09:33:04 +0200

    fixed quadratic form

--- Round4/quadratic-form-visualization.R
+++ Round4/quadratic-form-visualization.R
@@ -1,17 +1,16 @@
-n <- 100
+n <- 1e2
 p <- 2
-noise <- matrix(rnorm(n*p, sd=10), ncol=p, nrow=n)
-link <- apply(noise, 1, crossprod)
-plot(link, type='h')
-link2 <- link-median(link)
-plot(link2,type='h')
-probs <- plogis(link2)
+noise <- matrix(rnorm(n*p, sd=1), ncol=p, nrow=n)
+noise.augment <- augmentDesign(noise)
+B <- matrix(1, ncol = p, nrow = p)*2
+# plogis(colSums(c(0,0) * (B %*% c(0,0))))
+link <- -2+colSums(t(noise) * (B %*% t(noise)))
+probs <- plogis(link)
 plot(probs,type='h')
 labels <- rbinom(n, 1, probs)
-
 plot(noise, col=labels+1, pch=ifelse(labels,15,9), xlab=expression(x[1]), ylab=expression(x[2]))
 
-pdf(file = '~/workspace/permuting_accuracy/Round4/Output/quadratic-form.pdf')
-plot(noise, col=labels+1, pch=ifelse(labels,15,9), xlab=expression(x[1]), ylab=expression(x[2]))
-dev.off()
+# pdf(file = '~/workspace/permuting_accuracy/Round4/Output/quadratic-form.pdf')
+# plot(noise, col=labels+1, pch=ifelse(labels,15,9), xlab=expression(x[1]), ylab=expression(x[2]))
+# dev.off()
 
